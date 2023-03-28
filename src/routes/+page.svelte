a86d6104226aeddd1af6bd639e8a2fe5630e8faa
Hassaan Noor <fmoonlighth@gmail.com>
2023-03-29 03:43:27 +0500

    added wrong answer feedback & timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
     import { onMount, onDestroy } from 'svelte';
-    
+  
     let score = 0;
     let num1, num2, answer;
     let input = '';
     let fontSize = '6em';
+    let timer = 5;
+    let timerInterval;
+    let isBlinking = false;
     
     function generateQuestion() {
       num1 = Math.floor(Math.random() * 90) + 10;
@@ -16,8 +19,17 @@
       if (event.key === 'Enter') {
         if (parseInt(input) === answer) {
           score += 1;
+          timer -= 0.1;
+          if (timer <= 0) {
+            clearInterval(timerInterval);
+            alert(`Game Over! Your final score is ${score}`);
+          }
         } else {
           score -= 2;
+          isBlinking = true;
+          setTimeout(() => {
+            isBlinking = false;
+          }, 500);
         }
         input = '';
         generateQuestion();
@@ -28,16 +40,36 @@
     
     onMount(() => {
       generateQuestion();
+      timerInterval = setInterval(() => {
+        timer -= 0.1;
+        if (timer <= 0) {
+          clearInterval(timerInterval);
+          alert(`Game Over! Your final score is ${score}`);
+        }
+      }, 100);
       window.addEventListener('keydown', handleKeyPress);
     });
     
     onDestroy(() => {
+      clearInterval(timerInterval);
       window.removeEventListener('keydown', handleKeyPress);
     });
   </script>
   
-  <div style="font-size: {fontSize}; text-align: center;">
+  <div style="font-size: {fontSize}; text-align: center; {isBlinking ? 'animation: blink 0.5s linear infinite;' : ''}">
     {num1} + {num2} = {input}
+    
+    <style>
+      @keyframes blink {
+        50% {
+          color: red;
+        }
+      }
+    </style>
+  </div>
+  
+  <div style="position: absolute; top: 10px; left: 10px; font-size: 2em;">
+    Timer: {timer.toFixed(1)}
   </div>
   
   <div style="position: absolute; top: 10px; right: 10px; font-size: 2em;">
